82aaae5bac84a145b723f6b564f7f85a88c0b41e
VoTranQuocToan <votranquoctoan2004@gmail.com>
2026-04-14 14:52:43 +0700

    update strapi

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,10 +1,13 @@
-export default ({ env }: { env: any }) => ({
-    'strapi-llm-translator': {
-      enabled: true,
-      config: {
-        apiUrl: 'https://api.groq.com/openai/v1',
-        apiKey: env('GROQ_API_KEY'),
-        model: 'llama-3.3-70b-versatile',
+export default ({ env }: { env: any }) => {
+    console.log('GROQ_API_KEY value:', env('GROQ_API_KEY')); // debug
+    return {
+      'strapi-llm-translator': {
+        enabled: true,
+        config: {
+          apiUrl: 'https://api.groq.com/openai/v1',
+          apiKey: env('GROQ_API_KEY'),
+          model: 'llama-3.3-70b-versatile',
+        },
       },
-    },
-  });
+    };
+  };
