57cec0571514065b1dc2581c0407130916498537
VoTranQuocToan <votranquoctoan2004@gmail.com>
2026-04-14 14:29:13 +0700

    update strapi

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,3 @@
-import type { Core } from '@strapi/strapi';
-
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
-
 export default ({ env }: { env: any }) => ({
     'strapi-llm-translator': {
       enabled: true,
